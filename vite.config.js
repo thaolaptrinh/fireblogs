6f0e0169e209123bdfbb0f7994a53082790c0501
Nguyen Van Thao <thaolaptrinh@gmail.com>
2023-05-14 04:43:06 +0700

    modified

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import vueSvgLoader from "vite-svg-loader";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueSvgLoader()],
+});
