3fe2707d89cd1f63b7c956c7933eb0aab40ee82e
Nguyen Van Thao <thaolaptrinh@gmail.com>
2023-05-14 04:13:30 +0700

    component post

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="blog-wrapper">
+    <div class="blog-content">
+      <div>
+        <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
+
+        <h2 v-else>
+          {{ post.title }}
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  post: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
