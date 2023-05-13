70b1f19d8acdd7c33c4e80594b5e1bad0e2be49b
Nguyen Van Thao <thaolaptrinh@gmail.com>
2023-05-14 04:31:44 +0700

    add layouts default & auth

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <navigation />
+    <router-view></router-view>
+    <footer-app />
+  </div>
+</template>
+
+<script setup>
+import FooterApp from "../components/Footer.vue";
+import Navigation from "../components/Navigation.vue";
+</script>
+
+<style lang="scss" scoped></style>
