ff75f0834673ec33dd276b78b50d2bfea20f21ee
Nguyen Van Thao <thaolaptrinh@gmail.com>
2023-05-21 00:53:35 +0700

    create component loading & modal

---
+++ src/components/Loading.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="loading">
+    <span></span>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.loading {
+  position: fixed;
+  top: 0;
+  height: 100%;
+  z-index: 100;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @keyframes spin {
+    to {
+      transform: rotateZ(1turn);
+    }
+  }
+  span {
+    display: block;
+    width: 60px;
+    height: 60px;
+    border: 3px solid transparent;
+    border-top-color: #fff;
+    border-bottom-color: #fff;
+    border-radius: 50%;
+    animation: spin ease 1s infinite;
+  }
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="modal">
+    <div class="modal-content">
+      <p>{{ props.msg }}</p>
+      <button class="button" @click="closeModal">Close</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  message: {
+    type: String,
+    require: true,
+  },
+});
+const emit = defineEmits(["close-modal"]);
+const closeModal = () => {
+  emit("close-modal");
+};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+
+  &-content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    border-radius: 8px;
+    width: 300px;
+    padding: 40px 30px;
+    background-color: #fff;
+
+    p {
+      text-align: center;
+    }
+
+    button {
+      align-self: center;
+    }
+  }
+}
+</style>
