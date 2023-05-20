5478009962a8b862fc3f0dac689428aa582f2b91
Nguyen Van Thao <thaolaptrinh@gmail.com>
2023-05-20 21:07:13 +0700

    new view forgot password

---
+++ src/views/forgot-password.vue
@@ -0,0 +1,38 @@
+<template>
+  <form class="reset">
+    <h2>Reset Password</h2>
+    <p>Forgot your password? Enter your email to reset it</p>
+    <div class="inputs">
+      <div class="input">
+        <input type="text" placeholder="Email" v-model="form.email" />
+        <email-icon class="icon" />
+      </div>
+    </div>
+
+    <button class="button">Reset</button>
+    <div class="angle"></div>
+  </form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import EmailIcon from "../assets/Icons/envelope-regular.svg";
+import PasswordIcon from "../assets/Icons/lock-alt-solid.svg";
+import UserIcon from "../assets/Icons/user-alt-light.svg";
+
+const form = ref({
+  email: "",
+});
+</script>
+
+<style lang="scss" scoped>
+.reset {
+  h2 {
+    margin-bottom: 8px;
+  }
+  p {
+    text-align: center;
+    margin-bottom: 32px;
+  }
+}
+</style>
